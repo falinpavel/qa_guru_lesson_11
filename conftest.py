a592e74c9518c62c91c9909a55094c4e5125a9a8
Falin Pavel <falinpavel96@gmail.com>
2025-08-15 14:41:14 +0300

    +5

--- conftest.py
+++ conftest.py
@@ -1,33 +1,35 @@
 import pytest
 from selene import Browser, Config
 
-from selene.support.shared import browser
+# from selene.support.shared import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-from const import RESOURCES_DIR
+# from const import RESOURCES_DIR
 
 
 @pytest.fixture(scope="function", autouse=True)
 def browser_options() -> Browser:
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    driver_options.add_argument("--disable-gpu")
-    driver_options.add_argument("--ignore-certificate-errors")
-    driver_options.add_argument("--window-size=1920,1080")
-    driver_options.add_argument("--disable-extensions")
-    driver_options.add_argument("--disable-popup-blocking")
-    driver_options.add_argument("--disable-notifications")
-    driver_options.add_argument("--disable-infobars")
-    prefs = {
-        "profile.default_content_settings.popups": 0,
-        "profile.default_content_setting_values.notifications": 2,
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False,
-        "download.default_directory": RESOURCES_DIR,
-        "download.prompt_for_download": False,
-        "safebrowsing.enabled": True
-    }
-    driver_options.add_experimental_option("prefs", prefs)
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.page_load_strategy = 'eager'
+    # driver_options.add_argument("--disable-gpu")
+    # driver_options.add_argument("--ignore-certificate-errors")
+    # driver_options.add_argument("--window-size=1920,1080")
+    # driver_options.add_argument("--disable-extensions")
+    # driver_options.add_argument("--disable-popup-blocking")
+    # driver_options.add_argument("--disable-notifications")
+    # driver_options.add_argument("--disable-infobars")
+    # prefs = {
+    #     "profile.default_content_settings.popups": 0,
+    #     "profile.default_content_setting_values.notifications": 2,
+    #     "credentials_enable_service": False,
+    #     "profile.password_manager_enabled": False,
+    #     "download.default_directory": RESOURCES_DIR,
+    #     "download.prompt_for_download": False,
+    #     "safebrowsing.enabled": True
+    # }
+    driver_options = Options()
+    # driver_options.add_experimental_option("prefs", prefs)
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "128.0",
