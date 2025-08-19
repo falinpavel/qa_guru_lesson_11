7b08fb7b9f85731124392bfe50e1e14ae7a59c09
falinpavel-macbook <aloverapf@yandex.ru>
2025-08-19 22:50:34 +0300

    fix flaky test

--- helpers/pages/forms/practice_form/page_practice_form.py
+++ helpers/pages/forms/practice_form/page_practice_form.py
@@ -59,8 +59,8 @@ class PracticeFormPage:
             for hobby in self.user.hobbies:
                 # browser.all('label[class="custom-control-label"]').element_by(
                 #     have.text(hobby)).should(be.visible).should(be.clickable).click().should(be.enabled)
-                browser.element('#hobbiesWrapper').should(
-                    be.visible).element(by.text(hobby)).should(be.clickable).click().should(be.enabled)
+                browser.element('//*[@id="hobbiesWrapper"]/div[2]').should(
+                    be.visible).element(have.text(hobby)).should(be.clickable).click().should(be.enabled)
         with allure.step("Filling #uploadPicture field"):
             browser.element('#uploadPicture').perform(command.js.scroll_into_view).send_keys(Const.UPLOADED_FILE)
         with allure.step("Filling #currentAddress field"):
